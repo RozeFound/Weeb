1f10e8989c0427becde2215fb05192174fb54940
RozeFound <RozeFound@proton.me>
2024-04-28 21:47:09 +0700

    Add `rating:sensitive` to test list of tags

--- weeb/frontend/views/board.py
+++ weeb/frontend/views/board.py
@@ -45,7 +45,7 @@ class Board(Adw.Bin):
 
         self.manager = ProvidersManager()
 
-        GLib.timeout_add_seconds(2, self.search_by_tags, ["1girl", "1boy"])
+        GLib.timeout_add_seconds(2, self.search_by_tags, ["1girl", "1boy", "rating:sensitive"])
 
     def search_by_tags(self, tags: list[str]) -> None:
 
